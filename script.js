b47fbd2fe37d4f71d108c45c6b38a7716eac8953
Azegza <136713537+Azegza@users.noreply.github.com>
2024-04-21 17:39:43 +0100

    Update script.js
    updated the conversion functions

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// 
+
+ // <script>
+  //      function convertF(){
+   //             var c = document.getElementById("celsius").value;
+//                var f = (c * 9/5)+32
+  //              document.getElementById("fahrenheit").value = f;
+
+
+    //    }
+   //     function convertKg(){
+     //           var kg = document.getElementById("kg").value;
+     //           var pd = (kg * 2.2)
+     //           document.getElementById("pd").value = pd;
+
+
+   //     }
+   //     function convertM(){
+    //            var km = document.getElementById("km").value;
+     //           var ml = km * 0.62137
+      //          document.getElementById("ml").value = ml;
+
+
+    //    }
+//
+  //  </script>
